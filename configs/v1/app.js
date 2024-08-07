5ddcb21479a090b31b62cff064b1420a4d4a827a
GodwinOwonam <godwin.owonamg5@gmail.com>
2024-08-07 20:45:18 +0530

    FEAT: Implemented application logging

--- configs/v1/app.js
+++ configs/v1/app.js
@@ -1,3 +1,22 @@
-const AppConfig = {};
+const AppConfig = {
+  logging: {
+    logLevels: [
+      'error',
+      'warn',
+      'info',
+      'debug',
+      'verbose',
+      'silly'
+    ],
+    logLevel: {
+      ERROR: 'error',
+      WARN: 'warn',
+      INFO: 'info',
+      DEBUG: 'debug',
+      VERBOSE: 'verbose',
+      SILLY: 'silly'
+    }
+  }
+};
 
 module.exports = AppConfig;
