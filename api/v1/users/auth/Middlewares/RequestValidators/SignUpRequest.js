346e7726a6f1db775d792edfda20102ad9b0aa8d
GodwinOwonam <godwin.owonamg5@gmail.com>
2024-08-07 18:47:20 +0530

    FEAT: Strengthen user password on signup

--- api/v1/users/auth/Middlewares/RequestValidators/SignUpRequest.js
+++ api/v1/users/auth/Middlewares/RequestValidators/SignUpRequest.js
@@ -7,7 +7,15 @@ class SignUpRequest extends BaseValidator {
       lastName: 'required|string',
       username: 'required|string',
       email: 'required|string|email',
-      password: 'required|string',
+      password: [
+        'required',
+        'string',
+        {"min": 8},
+        {
+          "regex":
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z\\d\\s:])\\S{8,}$"
+        }
+      ],
       passwordConfirmation: 'required|string',
     };
   }
@@ -16,7 +24,8 @@ class SignUpRequest extends BaseValidator {
     return {
       required: ':attribute is required',
       string: ':attribute must be a string',
-      numeric: ':attribute must be a number'
+      numeric: ':attribute must be a number',
+      regex: ':attribute must be at least 8 characters long, must contain at least one uppercase letter, one lowercase letter, one number and one special character',
     };
   }
 }
